88be12db9b323226fc6af0b2f48c113117872a0e
Deviser <8338168+deviserops@users.noreply.github.com>
2025-01-12 03:27:37 +0530

    Update gameriz.js

--- js/gameriz.js
+++ js/gameriz.js
@@ -1,14 +1,1 @@
-$(document).ready(function () {
-    var csrf_token = $(document).find('meta[name="csrf-token"]').attr('content')
-    let noty = new Notify()
-    new Justify({
-        justifyError: true,
-        showBorderError: true,
-        underfieldError: true,
-        csrfTokenName: 'csrfmiddlewaretoken',
-        csrfToken: csrf_token,
-        customJustify: function (type, message) {
-            noty.show(type, message)
-        }
-    })
-})
+$(document).ready(function(){var r=$(document).find('meta[name="csrf-token"]').attr("content");let e=new Notify;new Justify({justifyError:!0,showBorderError:!0,underfieldError:!0,csrfTokenName:"csrfmiddlewaretoken",csrfToken:r,customJustify:function(r,n){e.show(r,n)}})});
